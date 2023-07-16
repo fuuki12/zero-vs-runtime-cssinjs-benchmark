de9b11b93a37435528f1b098310fa6f79c7bf539
fuki_fuki <fuuki1228@icloud.com>
2023-07-16 17:43:59 +0900

    1000 -> 10000個数を変更

--- cssinjs-benchmark/src/zeroruntime/TailwindComponent.tsx
+++ cssinjs-benchmark/src/zeroruntime/TailwindComponent.tsx
@@ -5,7 +5,7 @@ export const TailwindComponent = () => {
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
       <div>
         <div className="text-xl font-medium text-black">Tailwind CSS</div>
-        {[...Array(1000)].map((_, i) => (
+        {[...Array(10000)].map((_, i) => (
           <p className="text-gray-500" key={i}>
             You are using Tailwind CSS for styling this component.
           </p>
